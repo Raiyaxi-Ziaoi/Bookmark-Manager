3e981cb13eb4813e93d8b4c2f58d72b5cb9ded9d
DadaLittleOne <82474669+DadaLittleOne@users.noreply.github.com>
2022-06-29 00:53:03 +0100

    Empty File Error Fixed
    When the file is empty, it would cause an error due to how the file was read and written to.

--- main.py
+++ main.py
@@ -14,7 +14,10 @@ def write():  # Writing new bookmarks to file
         with open("bookmarks.txt", "a") as f:
             label = input("\nPlease input label for bookmark: ")
             url = input("\nPlease input URL for bookmark: ")
-            f.write(f"\n{label},{url}")
+            if os.stat("bookmarks.txt").st_size == 0:
+                f.write(f"{label},{url}")
+            else:
+                f.write(f"\n{label},{url}")
     except FileNotFoundError:
         open("bookmarks.txt", "x")
         write()
@@ -22,34 +25,38 @@ def write():  # Writing new bookmarks to file
 
 
 def bookmarks():  # Display existing bookmarks
-    try:
+    try:        
         with open("bookmarks.txt", "r") as f:
             print()
-            text = f.read()
-            text = text.split("\n")
+            if os.stat("bookmarks.txt").st_size == 0:
+                pause = input("\nThe file is empty. Please press enter to return to menu.")
+                choose()
+            else:
+                text = f.read()
+                text = text.split("\n")
 
-            i = 1
-            ulist = []
-            for item in text:
-                item = item.split(",")
-                if i < 10:
-                    print(f"0{i}. {item[0]}")
-                else:
-                    print(f"{i}. {item[0]}")
-                ulist.append(item[1].split("\n"))
-                i += 1
-            while True:
-                exceptionPass = True
-                choice = input("\nEnter bookmark to open: ")
-                try:
-                    choice = int(choice)
-                except Exception:
-                    exceptionPass = False
-                if exceptionPass and choice - 1 <= len(ulist) and choice - 1 >= 0:
-                    webbrowser.open(ulist[choice - 1][0])
-                    break
-                else:
-                    print("\nPlease enter valid input")
+                i = 1
+                ulist = []
+                for item in text:
+                    item = item.split(",")
+                    if i < 10:
+                        print(f"0{i}. {item[0]}")
+                    else:
+                        print(f"{i}. {item[0]}")
+                    ulist.append(item[1].split("\n"))
+                    i += 1
+                while True:
+                    exceptionPass = True
+                    choice = input("\nEnter bookmark to open: ")
+                    try:
+                        choice = int(choice)
+                    except Exception:
+                        exceptionPass = False
+                    if exceptionPass and choice - 1 <= len(ulist) and choice - 1 >= 0:
+                        webbrowser.open(ulist[choice - 1][0])
+                        break
+                    else:
+                        print("\nPlease enter valid input")
     except FileNotFoundError:
         open("bookmarks.txt", "x")
         bookmarks()
