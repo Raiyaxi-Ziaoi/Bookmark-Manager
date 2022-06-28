c07b636bd68c5700a778e94b9fb9c0f1ead4d96b
DadaLittleOne <82474669+DadaLittleOne@users.noreply.github.com>
2022-06-28 22:03:48 +0100

    initial commit
    initial commit of project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Bookmark Manager
- A customisable bookmark manager made in Python
+
+A customisable bookmark manager made in Python made by Raiyaxi Ziaoi
+The bookmark.txt file contains sample websites and more can be added via the application itself. It is also automatically formatted by the program when you use its in-built bookmark adder.
+
+This project was mostly created for fun but was also created on the basis that some browsers may use bookmarks to track us. Note: I am a beginner to Python so feel free to edit and optimise the code.
---
+++ main.py
@@ -0,0 +1,61 @@
+import os
+import webbrowser
+from sys import platform
+
+
+def leave():  # Exits program
+    print("\nThank you for using Bookmark Manager")
+    pause = input("\nPress enter to continue...")
+    exit()
+
+
+def write():  # Writing new bookmarks to file
+    with open("bookmarks.txt", "a") as f:
+        label = input("\nPlease input label for bookmark: ")
+        url = input("\nPlease input URL for bookmark: ")
+        f.write(f"\n{label},{url}")
+
+
+def bookmarks():  # Display existing bookmarks
+    with open("bookmarks.txt", "r") as f:
+        print()
+        text = f.read()
+        text = text.split("\n")
+
+        i = 1
+        ulist = []
+        for item in text:
+            item = item.split(",")
+            if i < 10:
+                print(f"0{i}. {item[0]}")
+            else:
+                print(f"{i}. {item[0]}")
+            ulist.append(item[1].split("\n"))
+            i += 1
+        while True:
+            choice = input("\nEnter bookmark to open: ")
+            choice = int(choice)
+            if isinstance(choice, int) and choice - 1 <= len(ulist) and choice - 1 >= 0:
+                webbrowser.open(ulist[choice - 1][0])
+                break
+            else:
+                print("\nPlease enter valid input")
+
+
+def choose():  # Input validated menu
+    print("\nEnter Choice:\n1. Write New Bookmark\n2. Open Bookmarks\n3. Exit")
+    choice = input()
+    if choice == "1":
+        write()
+        choose()
+    elif choice == "2":
+        bookmarks()
+    elif choice == "3":
+        leave()
+    else:
+        print("\nPlease enter valid input")
+        choose()
+
+
+print("Welcome to Bookmark Manager\n")
+choose()
