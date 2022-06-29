c651e28a9ded3502ade39361d59c0ce8de714464
DadaLittleOne <82474669+DadaLittleOne@users.noreply.github.com>
2022-06-29 21:00:22 +0100

    Delete Logic Error
    When deleting, it would delete the file and not re-create it. Name changes to folderPath to folderName

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import os
 import webbrowser
 from sys import platform
 
-folderPath= "bookmarks.txt"
+folderName= "bookmarks.txt"
 
 def delete():
     while True:
         print("\nAre you sure you want to delete all bookmarks? (Y / N):")
         yn = input()
         if yn.lower() == "y":
-            os.remove(folderPath)
+            os.remove(folderName)
+            open(folderName, "x")
             break
         elif yn.lower() == "n":
             break
@@ -25,23 +26,23 @@ def leave():  # Exits program
 
 def write():  # Writing new bookmarks to file
     try:
-        with open(folderPath, "a") as f:
+        with open(folderName, "a") as f:
             label = input("\nPlease input label for bookmark: ")
             url = input("\nPlease input URL for bookmark: ")
-            if os.stat(folderPath).st_size == 0:
+            if os.stat(folderName).st_size == 0:
                 f.write(f"{label},{url}")
             else:
                 f.write(f"\n{label},{url}")
     except FileNotFoundError:
-        open(folderPath, "x")
+        open(folderName, "x")
         write()
 
 
 def bookmarks():  # Display existing bookmarks
     try:        
-        with open(folderPath, "r") as f:
+        with open(folderName, "r") as f:
             print()
-            if os.stat(folderPath).st_size == 0:
+            if os.stat(folderName).st_size == 0:
                 pause = input("\nThe folder is empty. Please press enter to return to menu.")
                 choose()
             else:
@@ -71,15 +72,15 @@ def bookmarks():  # Display existing bookmarks
                     else:
                         print("\nPlease enter valid input")
     except FileNotFoundError:
-        open(folderPath, "x")
+        open(folderName, "x")
         bookmarks()
 
 def folderChoose():
     while True:
         folderIn = input("\nWhich folder do you want to open?: ")
         if os.path.exists(folderIn + ".txt"): 
-            global folderPath
-            folderPath = folderIn + ".txt"
+            global folderName
+            folderName = folderIn + ".txt"
             break
         else:
             print("\nFolder does not exist")
